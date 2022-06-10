d800bc849afb52064ef322685813ecbd3eca5c65
TaliaFlor <talianadegia@gmail.com>
2022-06-10 15:46:12 -0300

    add images

---
+++ README.md
@@ -0,0 +1,5 @@
+# OpenCV Image Blur Test
+
+A simple program to test OpenCV image blur filters using `mean (blur)` and `median (medianBlur)` filters.
+
+Based on [this code](https://docs.opencv.org/4.5.5/dc/dd3/tutorial_gausian_median_blur_bilateral_filter.html) on the OpenCV documentation.
--- src/main.py
+++ src/main.py
@@ -1,38 +1,42 @@
 import sys
+
 import cv2 as cv
 import numpy as np
 
 #  Global Variables
 DELAY_CAPTION = 1500
-DELAY_BLUR = 100
+DELAY_BLUR = 150
 MAX_KERNEL_LENGTH = 31
-src = None
-dst = None
+src_image = None
+dst_image = None
 window_name = 'Smoothing Demo'
+default_image = './data/peppers_gray_ruido.bmp'
 
 
-def main(argv):
+def main(argv) -> int:
     cv.namedWindow(window_name, cv.WINDOW_AUTOSIZE)
+
     # Load the source image
-    imageName = argv[0] if len(argv) > 0 else 'lena.jpg'
-    global src
-    src = cv.imread(cv.samples.findFile(imageName))
-    if src is None:
+    image_name = argv[0] if len(argv) > 0 else default_image
+    global src_image
+    src_image = cv.imread(cv.samples.findFile(image_name))
+    if src_image is None:
         print('Error opening image')
-        print('Usage: smoothing.py [image_name -- default ../data/lena.jpg] \n')
+        print(f"Usage: smoothing.py [image_name -- default {default_image}] \n")
         return -1
     if display_caption('Original Image') != 0:
         return 0
-    global dst
-    dst = np.copy(src)
+    global dst_image
+    dst_image = np.copy(src_image)
     if display_dst(DELAY_CAPTION) != 0:
         return 0
+
     # Applying Homogeneous blur
     if display_caption('Homogeneous Blur') != 0:
         return 0
 
     for i in range(1, MAX_KERNEL_LENGTH, 2):
-        dst = cv.blur(src, (i, i))
+        dst_image = cv.blur(src_image, (i, i))
         if display_dst(DELAY_BLUR) != 0:
             return 0
 
@@ -41,7 +45,7 @@ def main(argv):
         return 0
 
     for i in range(1, MAX_KERNEL_LENGTH, 2):
-        dst = cv.GaussianBlur(src, (i, i), 0)
+        dst_image = cv.GaussianBlur(src_image, (i, i), 0)
         if display_dst(DELAY_BLUR) != 0:
             return 0
 
@@ -50,7 +54,7 @@ def main(argv):
         return 0
 
     for i in range(1, MAX_KERNEL_LENGTH, 2):
-        dst = cv.medianBlur(src, i)
+        dst_image = cv.medianBlur(src_image, i)
         if display_dst(DELAY_BLUR) != 0:
             return 0
 
@@ -59,7 +63,7 @@ def main(argv):
         return 0
 
     for i in range(1, MAX_KERNEL_LENGTH, 2):
-        dst = cv.bilateralFilter(src, i, i * 2, i / 2)
+        dst_image = cv.bilateralFilter(src_image, i, i * 2, i / 2)
         if display_dst(DELAY_BLUR) != 0:
             return 0
 
@@ -68,20 +72,26 @@ def main(argv):
     return 0
 
 
-def display_caption(caption):
-    global dst
-    dst = np.zeros(src.shape, src.dtype)
-    rows, cols, _ch = src.shape
-    cv.putText(dst, caption,
-               (int(cols / 4), int(rows / 2)),
-               cv.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255))
+def display_caption(caption: str):
+    global dst_image
+    dst_image = np.zeros(src_image.shape, src_image.dtype)
+    rows, cols, _ch = src_image.shape
+    cv.putText(
+        dst_image,
+        caption,
+        (int(cols / 4), int(rows / 2)),
+        cv.FONT_HERSHEY_COMPLEX,
+        1,
+        (255, 255, 255)
+    )
     return display_dst(DELAY_CAPTION)
 
 
-def display_dst(delay):
-    cv.imshow(window_name, dst)
+def display_dst(delay: int) -> int:
+    cv.imshow(window_name, dst_image)
     c = cv.waitKey(delay)
-    if c >= 0: return -1
+    if c >= 0:
+        return -1
     return 0
 
 
